ac1356cf12d22084d7d10db766f7d06cb2226614
Joel Aduma <caesaragen@gmail.com>
2021-11-07 13:10:24 +0300

    add models

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
+const dotenv = require("dotenv")
+const mongoose = require('mongoose')
 
-app.listen('5000', () => {
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("connected to mongo")).catch(err=>console.log(err));
+
+app.listen('6000', () => {
     console.log("Backend is running")
 })
