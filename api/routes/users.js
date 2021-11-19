de7d62241ecc2f82e62b95737781dad81b188e08
Joel Aduma <caesaragen@gmail.com>
2021-11-19 13:24:56 +0300

    delete users

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Post = require('../models/Post');
 const bcrypt = require('bcrypt');
 
 //UPDATE
@@ -12,8 +13,8 @@ router.put("/:id", async (req, res) => {
       try {
           const updatedUser = await User.findByIdAndUpdate(req.params.id, {
               $set: req.body,
-          });
-          res.status(200).json("User updated!")
+          },{new:true});
+          res.status(200).json(updatedUser);
     } catch (err) {
       res.status(500).json(err);
     }
@@ -22,5 +23,23 @@ router.put("/:id", async (req, res) => {
   }
 });
 //DELETE
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      try {
+            await Post.deleteMany({username:user.username})
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("User has been deleted...");
+          } catch (err) {
+            res.status(500).json(err);
+          }
+    } catch (err) {
+      res.status(404).json("User not found...")
+    }
 
+  } else {
+    res.status(401).json("You do not have access to this account");
+  }
+});
 module.exports = router;
