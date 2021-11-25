6dd63077cdf1b2a9b7a103cf2a1d26afbd499c5f
Joel Aduma <caesaragen@gmail.com>
2021-11-25 17:07:52 +0300

    add transitions

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
     return (
         <div className="header">
             <div className="headerTitles">
-                <span className="headerTitleSm">React & Node</span>
+                <span className="headerTitleSm">Ragen's</span>
                 <span className="headerTitleLg">Blog</span>
             </div>
             <img src={comp} alt="" className="headerImg" />
