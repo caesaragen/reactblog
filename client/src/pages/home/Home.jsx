3944ba46c27664a414d8cfb3e5b536aa56645fe0
Joel Aduma <caesaragen@gmail.com>
2021-12-02 10:51:17 +0300

    fetch posts

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import Header from '../../components/header/Header'
 import Posts from '../../components/posts/Posts'
 import Sidebar from '../../components/sidebar/Sidebar'
 import "./home.css"
 
 export default function Home() {
+    const [posts, setPosts] = useState([])
+    useEffect(() => {
+          const fetchPosts = async () => {
+              const res = await axios.get('/posts')
+              setPosts(res.data)
+            //   console.log(res)
+          }
+        fetchPosts();
+        console.log(posts)
+    }, [])
     return (
         <>
         <Header />
